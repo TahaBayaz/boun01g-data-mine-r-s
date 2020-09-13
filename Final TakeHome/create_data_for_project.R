55c5c12412919ec82598695c63a076f835f80cf5
Taha Bayaz <taha.bayaz@boun.edu.tr>
2020-09-13 18:14:56 +0300

    Correcting the las commit

--- Final TakeHome/create_data_for_project.R
+++ Final TakeHome/create_data_for_project.R
@@ -25,7 +25,7 @@ all_data = all_data %>%
   filter(!(is.na(brand_name) | brand_name == "TOPLAM:" | brand_name == "TOPLAM" | grepl("ODD", brand_name)))
 
 all_data = all_data %>%
-  mutate(ifelse(brand_name == "ASTON MARTÝN", "ASTON MARTIN", brand_name))
+  mutate(brand_name = ifelse(brand_name == "ASTON MARTÝN", "ASTON MARTIN", brand_name))
 
 saveRDS(all_data, file = "all_data")
 # all_data = readRDS("all_data")
